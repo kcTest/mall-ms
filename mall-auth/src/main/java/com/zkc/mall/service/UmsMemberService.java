0f3b80857c443887d36b4e519092ebd387a3f303
kcTest <763336180@qq.com>
2021-12-28 23:44:21 +0800

    Authorization Server Configuration

--- mall-auth/src/main/java/com/zkc/mall/service/UmsMemberService.java
+++ mall-auth/src/main/java/com/zkc/mall/service/UmsMemberService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "mall-auth")
+@FeignClient(value = "mall-portal")
 public interface UmsMemberService {
 	
 	@GetMapping("/portal/loadByUsername")
