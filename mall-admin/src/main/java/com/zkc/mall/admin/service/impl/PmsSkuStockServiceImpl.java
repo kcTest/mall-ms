3da052c9df17cdcf4bdff786cb2324ebc88098fe
kcTest <763336180@qq.com>
2022-02-17 18:47:41 +0800

    -skuStock getList

--- mall-admin/src/main/java/com/zkc/mall/admin/service/impl/PmsSkuStockServiceImpl.java
+++ mall-admin/src/main/java/com/zkc/mall/admin/service/impl/PmsSkuStockServiceImpl.java
@@ -3,14 +3,12 @@ package com.zkc.mall.admin.service.impl;
 import cn.hutool.core.util.StrUtil;
 import com.zkc.mall.admin.dao.PmsSkuStockDao;
 import com.zkc.mall.admin.service.PmsSkuStockService;
-import com.zkc.mall.mbg.mapper.PmsProductMapper;
 import com.zkc.mall.mbg.mapper.PmsSkuStockMapper;
-import com.zkc.mall.mbg.model.PmsProductExample;
 import com.zkc.mall.mbg.model.PmsSkuStock;
 import com.zkc.mall.mbg.model.PmsSkuStockExample;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 @Service
@@ -25,9 +23,9 @@ public class PmsSkuStockServiceImpl implements PmsSkuStockService {
 	@Override
 	public List<PmsSkuStock> getList(Long pid, String skuCode) {
 		PmsSkuStockExample example = new PmsSkuStockExample();
-		example.createCriteria().andIdEqualTo(pid);
+		PmsSkuStockExample.Criteria criteria = example.createCriteria().andProductIdEqualTo(pid);
 		if (!StrUtil.isEmpty(skuCode)) {
-			example.createCriteria().andSkuCodeLike("%" + skuCode + "%");
+			criteria.andSkuCodeLike("%" + skuCode + "%");
 		}
 		return skuStockMapper.selectByExample(example);
 	}
